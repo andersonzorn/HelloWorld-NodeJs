350e93f4e6b8152a731a97723f5e6a876c8e4abd
Anderson Zorn <andersonmeizorn@gmail.com>
2026-04-19 12:48:58 -0300

    feature/layout: novo visual

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const renderPage = (name) => `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Hello ${name} | Zorn é o cara</title>
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌍</text></svg>">
-    <style>
+    <style> 
       :root {
         color-scheme: dark;
         --bg: #03111f;
