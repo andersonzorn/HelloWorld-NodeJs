b4407a9bd8fe1adfd8e859469cad831b90a336f3
Anderson Zorn <andersonmeizorn@gmail.com>
2026-04-19 13:43:13 -0300

    feature/layout: novo visual

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const renderPage = (name) => `<!DOCTYPE html>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Hello ${name} | Zorn | </title>
+    <title>Hello ${name} | Zorn</title>
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌍</text></svg>">
     <style> 
       :root {
